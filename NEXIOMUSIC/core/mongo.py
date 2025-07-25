ff0ebe8412ea33c66a8aeb7415f2841b9b716e4b
ISTKHAR-CODER786 <istkharalam6202@gmail.com>
2025-07-25 16:54:33 +0530

    Update mongo.py

--- NEXIOMUSIC/core/mongo.py
+++ NEXIOMUSIC/core/mongo.py
@@ -7,7 +7,7 @@ from ..logging import LOGGER
 LOGGER(__name__).info("❖ CONNECTING TO YOUR MONGO DATABASE 💚")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+    mongodb = _mongo_async_.Yukki
     LOGGER(__name__).info("❖ CONNECTED TO YOUR MONGO DATABASE 💜")
 except:
     LOGGER(__name__).error("❖ FAILED TO CONNECT TO YOUR MONGO DATABASE 💛")
